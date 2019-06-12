9d0ff4163177031dbf37d0e61c6f4f8ffeb9fe10
Aarati <aarati@Aaratis-MacBook-Pro-2.local>
2019-06-12 14:47:34 +0800

    adds student presentation template

--- DOWNLOADS/EXAMPLES/data_example/main.js
+++ DOWNLOADS/EXAMPLES/data_example/main.js
@@ -1,3 +1,32 @@
-console.log(data)
+//console.log("good morning")
+// console.log(data[0]["shape"])
+// console.log(data[1]["shape"])
+// console.log(data[2]["shape"])
+// console.log(data[3]["shape"])
+// console.log(data[4]["shape"])
+// console.log(data[5]["shape"])
+// console.log(data[6]["shape"])
+// console.log(data[7]["shape"])
+// console.log(data[8]["shape"])
+// console.log(data[9]["shape"])
+
+
+var myRandomNumber = Math.floor(Math.random() * data.length - 1)
+var myRandomNumber1 = Math.floor(Math.random() * data.length - 1)
+var myRandomNumber2 = Math.floor(Math.random() * data.length - 1)
+var myRandomNumber3 = Math.floor(Math.random() * data.length - 1)
+var myRandomNumber4 = Math.floor(Math.random() * data.length - 1)
+console.log(data[myRandomNumber]["latitude"])
+console.log(data[myRandomNumber]["longitude"])
+
+console.log("----------------")
+console.log(data[myRandomNumber2]["latitude"])
+console.log(data[myRandomNumber2]["longitude"])
+console.log("----------------")
+//Math.floor(Math.random() * Math.floor(max));
 
 /* for loop */
+for (var i = data.length - 1; i >= 0; i--) {
+	//console.log(data[i]["shape"])
+}
+
